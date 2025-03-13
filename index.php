ff9048fe034d0be890fa86f89db1c5587789cc97
yoshimasa <yi@inakanomado.com>
2025-03-13 14:58:18 +0900

    SCSSミックスインを追加し、index.phpのテキストを更新

--- index.php
+++ index.php
@@ -37,18 +37,12 @@
 		</div>
 		<div class="right">
 			<h2>
-				丹波篠山オーガニックビレッジとは
+				オーガニックビレッジとは
 				<span class="flex"><img src="/wp-content/uploads/2023/03/en-01.png"></span>
 			</h2>
 			<div class="text relative">
 				オーガニックビレッジとは、農林水産省がモデル地区として掲げる、有機農業の生産から消費まで一貫し、農業者や事業者、地域住民を巻き込んだ取組を進める市町村のことです。<br>
-				丹波篠山市で、丹波篠山ワクワク農都づくり協議会※が「丹波篠山ワクワク有機農業実施計画～水と創る農都ものがたり～」を策定し、令和5年4月にオーガニックビレッジ宣言をしました。<br>
-				協議会では、実施計画の取組を実践し、有機農業に取組む人づくり、有機農業技術の確立、地域の魅力向上などにつなげ、伝統的な丹波篠山の農業に加えた新たな価値を創出します。
-
-				<div style="margin-top:10px; font-size:12px;color:#666;line-height:1.2;">
-					※ 丹波篠山ワクワク農都づくり協議会<br>
-					認定農業者連絡協議会、篠山自然派、丹波ささやま農業協同組合、丹波篠山市で構成。
-				</div>
+				オーガニックビレッジとして宣言した市町村は、全国で131市町村（2024年12月末日現在）。国では、2030年までに200のオーガニックビレッジ創出を目指しています。
 
 				<div class="back-text sp absolute"><img src="/wp-content/uploads/2023/03/sp-logo.png"></div>
 			</div>
@@ -119,16 +113,17 @@
 		</div>
 		<div class="right">
 			<h2>
-				オーガニックビレッジ宣言
+				丹波篠山市オーガニックビレッジ宣言
 				<span class="flex"><img src="/wp-content/uploads/2023/04/image001.png"></span>
 			</h2>
 			<div class="text">
-				令和5年4月27日、以下の内容で丹波篠山市長がオーガニックビレッジ宣言を行いました。<br><br>
-				兵庫県丹波篠山市は、瀬戸内海と日本海に流れ込む3つの河川の源流をもち、300年の歴史のある黒大豆など、伝統的な農産物の生産地です。盆地にひろがる「まほろば」〜里山からため池、水路、水田、川までのつながりが創り出した環境〜は、先人たちが大切にまもってきた農都・丹波篠山の宝です。<br>
-				本市では、ずっと地域の農業を支えてきた農家、市外から移住し就農を志すひと、大規模から小規模まで多様な農家が、農都ものがたりの主人公として、自然環境や生き物に配慮した農業を実践しています。<br>
-				そのひとつが有機農業であり、これからさらに農法や技術などについての情報交換や連携を進め、多様な農家が広く「実践できるかたち」を確立していきます。<br>
-				多様な農業があるからこそ、多くの消費者にさまざまな丹波篠山の農産物を届けることができます。持続可能な農業・農村を次世代に引き継ぐため、豊かな自然と伝統、そこに有機農業というワクワクする新たなチャレンジを加えた「水と創る農都ものがたり」に基づき、ここに「オーガニックビレッジ」を宣言します。
-				<div class="btn"><a class="flex relative" href="/wp-content/uploads/2023/04/オーガニックビレッジ宣言.pdf">PDFはこちら</a></div>
+				市内の認定農業者や小規模農業者などの多様な農業者で「丹波篠山ワクワク農都づくり協議会」をつくり、それぞれが自然環境や生きものに配慮した農業を進めることを有機農業実施計画に盛り込み、2023年4月27日、丹波篠山市はオーガニックビレッジを宣言しました。
+				令和5年4月27日、以下の内容で丹波篠山市長がオーガニックビレッジ宣言を行いました。
+				<div class="btn">
+					<a class="flex relative" href="/wp-content/uploads/2023/04/オーガニックビレッジ宣言.pdf">
+						丹波篠山市オーガニックビレッジ宣言PDFはこちら
+					</a>
+				</div>
 			</div>
 			<div class="cont">
 				有機農業実施計画
@@ -193,7 +188,7 @@
 		</div>
 		<div style="display:none;"><a href="#">お問い合わせはこちら</a></div>
 	</div>
-	
+
 	<div id="initiative-cont">
 		<div class="initiative-cont flex relative">
 			<div class="back-text absolute">
@@ -203,53 +198,202 @@
 				</picture>
 			</div>
 			<h2 class="relative">
-				取り組み事例
+				オーガニックビレッジ取り組み
 				<span class="flex"><img src="/wp-content/uploads/2023/03/en-09_1.png"></span>
 			</h2>
-			<div class="inner flex relative">
-				<div class="box flex">
-					<div class="image"><img src="/wp-content/uploads/2024/03/IMG_0583.jpg"></div>
-					<div class="content">
-						<div class="point">
-							<img src="/wp-content/uploads/2023/03/num01.png">
+			<div class="inner relative">
+				<h3 class="relative">水田除草機を使った水稲有機栽培実証</h3>
+
+				<div class="list flex">
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
 						</div>
-						<div class="head">
-							「作りたい人」づくり
-						</div>
-						<div class="text">
-							黒大豆では、慣行栽培からの転換実証を行い、有機栽培技術の情報収集を行いました。
-						</div>
-					</div>
-				</div>
-				<div class="box flex">
-					<div class="image"><img src="/wp-content/uploads/2024/03/IMG_7552.jpg"></div>
-					<div class="content">
-						<div class="point">
-							<img src="/wp-content/uploads/2023/03/num02.png">
-						</div>
-						<div class="head">
-							「みどりの技」づくり
-						</div>
-						<div class="text">
-							水稲の水田除草機の比較、スマート機器を使った生育調査を実施しました。
+						<div class="content">
+							<div class="head">
+								土谷 学さん
+								<span>（殿町）</span>
+							</div>
+							<div class="text">
+								「あめんぼ号」を使った除草の実証、自動で稲の草丈や茎数、葉色を測定するスマート機器を使った有機栽培の生育調査に取り組みました。<br>
+								実証の2年間で、機械操作が上達し、安定して収量をあげることができました。
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="box flex">
-					<div class="image"><img src="/wp-content/uploads/2024/03/S__215236614.jpg"></div>
-					<div class="content">
-						<div class="point">
-							<img src="/wp-content/uploads/2023/03/num03.png">
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
 						</div>
-						<div class="head">
-							「魅せる地域」づくり
+						<div class="content">
+							<div class="head">
+								代表 田渕 真也さん
+								<span>株式会社丹波たぶち農場（口阪本）</span>
+							</div>
+							<div class="text">
+								ガスが発生しやすい粘土質の土壌で、アイガモロボを使った実証に取り組みました。<br>
+								実証2年目は、苗の成長に合わせてアイガモロボを動かす時間や頻度を調整し、収量を増加させることができました。
+
+							</div>
 						</div>
-						<div class="text">
-							市内で開催されたマルシェに出店し、有機野菜を販売しました。
+					</div>
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								代表 原 智宏さん
+								<span>株式会社アグリヘルシーファーム（味間奥）</span>
+							</div>
+							<div class="text">
+								「WEEDMAN（ウィードマン）」を使った除草実証に取り組みました。<br>
+								実証した2年間では、安定した収量がとれ、今後さらに有機栽培面積の拡大と有機JAS取得を目指します。
+							</div>
 						</div>
 					</div>
 				</div>
 
+				<h3 class="relative">黒大豆有機栽培実証</h3>
+
+				<div class="list flex">
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								澤村 寛文さん
+								<span>丹波篠山伝田農産（後川上）</span>
+							</div>
+							<div class="text">
+								黒色、白色のビニールマルチや、緑肥を使った栽培に取り組みました。<br>
+								新たにアイガモ農法によるお米の有機栽培にも挑戦しています。
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								小前 芳彦さん
+								<span>（味間南）</span>
+							</div>
+							<div class="text">
+								篠山自然派代表。有機農業歴は50年以上、有機JASの検査員も務めています。<br>
+								土づくりと水管理に力を入れています。地域の多様な農業者との協調を大切しています。
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								代表 湊 友加さん
+								<span>農事組合法人なちゅらるはーもにー（垣屋）</span>
+							</div>
+							<div class="text">
+								市のオーガニックビレッジ宣言を機に、黒大豆の有機栽培に挑戦。市内有機農業者の栽培指導を受け、慣行栽培のほ場を、有機栽培のほ場へ転換する実証に取り組みました。実証2年目は、カメムシ被害を抑え、一定の収量が得られました。
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								中末 智己さん
+								<span>中末農園（大山上）</span>
+							</div>
+							<div class="text">
+								有機農業歴は20年以上。ミネラル、堆肥、緑肥を使った栽培を実践。新たに有機農業に取り組む農業者へのアドバイスや、土づくりの勉強会を開催するなど、有機農業者を増やす取組みに力を入れています。
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								吉良 佳晃さん
+								<span>丹波篠山　良農園（不来坂）</span>
+							</div>
+							<div class="text">
+								緑肥を使った栽培や、有機肥料の効果比較などを行いました。実証1年目で一定の収量が得られました。得られた情報や栽培事例を紹介し、有機農業者の仲間づくりを進めます。
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<h3 class="relative">有機農業・有機農産物の普及啓発</h3>
+
+				<div class="list flex">
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								学校給食への有機野菜の提供、食育授業など
+							</div>
+							<div class="text">
+								学校給食に、有機野菜も使用しています。市内小学校では、有機農業者による食育授業を実施しています。
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								勉強会・座談会の開催
+							</div>
+							<div class="text">
+								有機農法に関する勉強会を開催しています。<br>
+								また、有機農業や有機農産物に興味がある方を対象に、市内の有機農業者とお話ができる座談会も開催しています。
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								オーガニックテーブルマーケット
+							</div>
+							<div class="text">
+								丹波篠山市飲食業組合が、市内で生産された有機農産物を使った、お弁当やスイーツの販売、お料理ワークショップなど気軽に楽しめるイベントを開催しました（2024年11月30日）
+							</div>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="image">
+							<img src="/wp-content/uploads/2023/03/reimg-05.jpg">
+						</div>
+						<div class="content">
+							<div class="head">
+								マルシェや映画上映会の開催
+							</div>
+							<div class="text">
+								篠山自然派が市内でマルシェや直売会を開催しています。
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -273,7 +417,8 @@
 			</h2>
 			<div class="text">
 				<span>SNSで随時発信中！</span>
-				Instagramをフォローして<br>丹波篠山の有機農業についてもっと知りましょう。
+				Instagramで丹波篠山の有機農業の取り組みを紹介しています。<br>
+				丹波篠山の有機農業についてもっと知ってください。
 				<div class="image-list flex">
 					<?php echo do_shortcode('[instagram-feed feed=2]'); ?>
 				</div>
@@ -313,7 +458,7 @@
 			</div>
 			<div class="box flex">
 				<div class="logo flex"><img src="/wp-content/uploads/2023/03/logo-pg.png"></div>
-				<p>オーガニックを丹波篠山に落とし込んでみた時、分水嶺、次世代に残したい農業の都（農都）、集落で行う農業スタイル、生態系の循環のキーワードが出たんだ。</p>
+				<p>オーガニックという言葉を丹波篠山でイメージした時、分水嶺、次世代に残したい農業の都（農都）、集落で行う農業スタイル、生態系の循環のキーワードが出たんだ。</p>
 			</div>
 			<div class="box flex">
 				<div class="logo flex"><img src="/wp-content/uploads/2023/03/logo-po.png"></div>
@@ -321,7 +466,7 @@
 			</div>
 			<div class="box flex">
 				<div class="logo flex"><img src="/wp-content/uploads/2023/03/logo-pg.png"></div>
-				<p>キャッチフレーズの「水と創る農都ものがたり」は、水が作物・大地・人を創り上げ、丹波篠山らしい農都として、過去と現在と未来のものがたりを紡いでいこうという想いがこもっているよ。</p>
+				<p>キャッチフレーズの「水と創る農都ものがたり」は、水が作物・大地・人を創り上げ、農業の都（農都）として、過去と現在と未来のものがたりを紡いでいこうという想いがこもっているよ。</p>
 			</div>
 			<div class="box flex">
 				<div class="logo flex"><img src="/wp-content/uploads/2023/03/logo-po.png"></div>
